1e512c46e3db90bc24c9349aaef60d3ad49b6e4a
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-06-04 01:29:33 +0200

    rename. remove optional. exceptions

---
+++ src/main/java/io/github/fandreuz/fetch/FetchException.java
@@ -0,0 +1,19 @@
+package io.github.fandreuz.fetch;
+
+/**
+ * Marks an exception which occurred while fetching a remote dataset.
+ *
+ * @author fandreuz
+ */
+public class FetchException extends RuntimeException {
+
+    private static final long serialVersionUID = 0L;
+
+    public FetchException(String message) {
+        super(message);
+    }
+
+    public FetchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
