4bf7f89807bfd1f508d612e9c7af859db28b7964
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-06-04 10:33:38 +0200

    log exception before sending to the client

--- src/main/java/io/github/fandreuz/DatabaseExceptionMapper.java
+++ src/main/java/io/github/fandreuz/DatabaseExceptionMapper.java
@@ -4,6 +4,7 @@ import io.github.fandreuz.database.DatabaseException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Implementation of {@link ExceptionMapper} for {@link DatabaseException}.
@@ -11,10 +12,12 @@ import jakarta.ws.rs.ext.Provider;
  * @author fandreuz
  */
 @Provider
+@Slf4j
 public class DatabaseExceptionMapper implements ExceptionMapper<DatabaseException> {
 
     @Override
     public Response toResponse(DatabaseException exception) {
+        log.error("DatabaseException caught", exception);
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR) //
                 .entity(exception.getMessage()) //
                 .build();
--- src/main/java/io/github/fandreuz/FetchExceptionMapper.java
+++ src/main/java/io/github/fandreuz/FetchExceptionMapper.java
@@ -4,6 +4,7 @@ import io.github.fandreuz.fetch.FetchException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Implementation of {@link ExceptionMapper} for {@link FetchException}.
@@ -11,10 +12,12 @@ import jakarta.ws.rs.ext.Provider;
  * @author fandreuz
  */
 @Provider
+@Slf4j
 public class FetchExceptionMapper implements ExceptionMapper<FetchException> {
 
     @Override
     public Response toResponse(FetchException exception) {
+        log.error("FetchException caught", exception);
         return Response.status(Response.Status.BAD_REQUEST) //
                 .entity(exception.getMessage()) //
                 .build();
