12c1ab9c7644b28479e67e27251b20960449fec1
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-06-04 16:39:04 +0200

    two separate controllers for dataset and metadata

--- src/main/java/io/github/fandreuz/controller/DatasetResource.java
+++ src/main/java/io/github/fandreuz/controller/DatasetResource.java
@@ -3,19 +3,16 @@ package io.github.fandreuz.controller;
 import io.github.fandreuz.model.DatasetMetadata;
 import io.github.fandreuz.model.DatasetService;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import java.util.SortedSet;
 
 /**
  * Dataset resource controller.
  *
  * @author fandreuz
  */
-@Path("/metadata")
-public final class DatasetMetadataResource {
+@Path("/")
+public final class DatasetResource {
 
     @Inject
     private InputValidationService inputValidationService;
@@ -23,20 +20,11 @@ public final class DatasetMetadataResource {
     @Inject
     private DatasetService datasetService;
 
-    @POST
+    @PUT
     public DatasetMetadata create(DatasetLocator datasetLocator) {
         inputValidationService.validateInput(datasetLocator);
         return datasetService.createDataset(datasetLocator.getCollectionId(), datasetLocator.getFileName());
     }
 
-    @GET
-    @Path("{id}")
-    public DatasetMetadata get(@PathParam("id") String id) {
-        return datasetService.getMetadata(id);
-    }
-
-    @GET
-    public SortedSet<DatasetMetadata> list() {
-        return datasetService.getAllMetadata();
-    }
+    // TODO Endpoints for basic search & operations
 }
